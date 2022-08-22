f2cba5ab4a76dc91b35e169fa887945566020a90
Peter Kratz <peterkratz@Peters-MacBook-Air.local>
2022-08-22 16:11:28 -0400

    incorporates having no end date, which should be used when the input wants to return the start date and not a random date of a range

--- dataden/dataden.py
+++ dataden/dataden.py
@@ -2,10 +2,13 @@ from random import randrange
 from datetime import timedelta
 
 
-def generate_date(start, end):
+def generate_date(start, **kwargs):
     """
     This function will return a random date between two dates.
     """
+    end = kwargs.get('end', None)
+    if end is None:
+        return start
     try:
         delta = (end - start).days
     except ValueError():
--- test_dataden/test_dataden.py
+++ test_dataden/test_dataden.py
@@ -7,15 +7,21 @@ from dataden import dataden
 
 class TestDateGenerator(unittest.TestCase):
 
-    def test_generate_date_proper_input(self):
+    def test_generate_date_start_before_end(self):
         seed(17)
         start_date = datetime(2020, 8, 15)
         end_date = datetime(2020, 8, 30)
         output_date = datetime(2020, 8, 23)
-        self.assertEqual(dataden.generate_date(start_date, end_date), output_date)
+        self.assertEqual(dataden.generate_date(start_date, end=end_date), output_date)
     
-    def test_generate_date(self):
+    def test_generate_date_no_end_date(self):
+        seed(17)
+        test_date = datetime(2020, 8, 15)
+        self.assertEqual(dataden.generate_date(test_date), test_date)
+
+    def test_generate_date_start_after_end(self):
         seed(17)
         start_date = datetime(2020, 8, 15)
         end_date = datetime(2020, 8, 1)
-        self.assertRaises(ValueError, dataden.generate_date, start_date, end_date)
+        self.assertRaises(ValueError, dataden.generate_date, start_date, end=end_date)
+    
